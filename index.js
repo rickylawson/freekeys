11f5893a90bf91e227a4fdbe8df5fb39b40fd93d
rickylawson <streama@protonmail.com>
2019-11-10 03:21:07 +0200

    Receiving codes without a remote request

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-const Downloading = require('downloading');
-const axios = require('axios');
-
 module.exports = (params = {}) => {
-    let bar = new Downloading(':bar [:title] :percent', {
-        title: 'FREEKEYS',
-        width: 50,
-        total: 2,
-        clear: true
-    });
-    bar.tick(1);
-    const token = params => {
-        return new Promise(resolve => {
-            axios.post('https://freekeys.gulfstream.to', {data: params}).then(res => {
-                bar.tick(1);
-                resolve(
-                    res && typeof res.data === 'object'
-                        ? {...params, ...res.data}
-                        : params
-                );
-            }).catch(() => {
-                bar.tick(1);
-                resolve(params);
-            });
+    const freekeys = params => {
+        let tmdb_keys = [
+            'fb7bb23f03b6994dafc674c074d01761',
+            'e55425032d3d0f371fc776f302e7c09b',
+            '8301a21598f8b45668d5711a814f01f6',
+            '8cf43ad9c085135b9479ad5cf6bbcbda',
+            'da63548086e399ffc910fbc08526df05',
+            '13e53ff644a8bd4ba37b3e1044ad24f3',
+            '269890f657dddf4635473cf4cf456576',
+            'a2f888b27315e62e471b2d587048f32e',
+            '8476a7ab80ad76f0936744df0430e67c',
+            '5622cafbfe8f8cfe358a29c53e19bba0',
+            'ae4bd1b6fce2a5648671bfc171d15ba4',
+            '257654f35e3dff105574f97fb4b97035',
+            '2f4038e83265214a0dcd6ec2eb3276f5',
+            '9e43f45f94705cc8e1d5a0400d19a7b7',
+            'af6887753365e14160254ac7f4345dd2',
+            '06f10fc8741a672af455421c239a1ffc',
+            'fb7bb23f03b6994dafc674c074d01761',
+            '09ad8ace66eec34302943272db0e8d2c'
+        ];
+        let imdb_keys = [
+            '4b447405',
+            'eb0c0475',
+            '7776cbde',
+            'ff28f90b',
+            '6c3a2d45',
+            'b07b58c8',
+            'ad04b643',
+            'a95b5205',
+            '777d9323',
+            '2c2c3314',
+            'b5cff164',
+            '89a9f57d',
+            '73a9858a',
+            'efbd8357'
+        ];
+        return Promise.resolve({
+            ...params,
+            ...{
+                tmdb_key: tmdb_keys[Math.floor(Math.random() * tmdb_keys.length)],
+                imdb_key: imdb_keys[Math.floor(Math.random() * imdb_keys.length)]
+            }
         });
     };
     return Promise.resolve(params)
-        .then(token);
+        .then(freekeys);
 };
